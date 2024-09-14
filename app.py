41d7f7e473f69a8d05934ebb9a1cb2b4483dfc79
dvanael <anael.noki@gmail.com>
2024-09-14 14:56:03 -0300

    refactor: atualiza `def boletim` e templates
    esse commit cria a classe User para facilitar as requisições aos
    endpoints da apo, atualiza templates para nova logica do app.py

--- app.py
+++ app.py
@@ -21,35 +21,70 @@ oauth.register(
 )
 
 
+class User:
+    def __init__(self, oauth):
+        self.oauth = oauth
+
+    def get_user_data(self):
+        return self.oauth.suap.get('v2/minhas-informacoes/meus-dados').json()
+
+    def get_boletim(self, ano_letivo, periodo_letivo):
+        return self.oauth.suap.get(f"v2/minhas-informacoes/boletim/{ano_letivo}/{periodo_letivo}/").json()
+
+    def get_periodos(self):
+        return self.oauth.suap.get("v2/minhas-informacoes/meus-periodos-letivos/").json()
+
+
 @app.route('/')
 def index():
     if 'suap_token' in session:
-        meus_dados = oauth.suap.get('v2/minhas-informacoes/meus-dados')
-        return render_template('user.html', user_data=meus_dados.json())
+        suap_user = User(oauth)
+        user = suap_user.get_user_data()
+        return render_template('user.html', user=user)
     else:
         return render_template('index.html')
 
-@app.route("/boletim/")
-def boletim():
-    meus_dados = oauth.suap.get('v2/minhas-informacoes/meus-dados')
-    boletim = oauth.suap.get('v2/minhas-informacoes/meus-diarios/2022/1/')
-    print(boletim.json())
-    return render_template("boletim.html", user_data=meus_dados.json(), boletim=boletim.json())
 
-@app.route('/login')
+@app.route("/boletim/", methods=["GET", "POST"])
+def boletim():
+    suap_user = User(oauth)
+
+    if request.method == "POST":
+        periodo = request.form["periodo"]
+        return redirect(url_for("boletim", periodo=periodo))
+
+    if request.method == "GET":
+        periodo = request.args.get("periodo", "2024.1")
+
+    ano_letivo, periodo_letivo = periodo.split(".")
+
+    user = suap_user.get_user_data()
+    boletim = suap_user.get_boletim(ano_letivo, periodo_letivo)
+    periodos = suap_user.get_periodos()
+
+    context = {
+        "user": user,
+        "boletim": boletim,
+        "periodos": periodos,
+        "selected_periodo": periodo
+    }
+
+    return render_template("boletim.html", context=context)
+
+
+@app.route('/login/')
 def login():
     redirect_uri = url_for('auth', _external=True)
-    print(redirect_uri)
     return oauth.suap.authorize_redirect(redirect_uri)
 
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     session.pop('suap_token', None)
     return redirect(url_for('index'))
 
 
-@app.route('/login/authorized')
+@app.route('/login/authorized/')
 def auth():
     token = oauth.suap.authorize_access_token()
     session['suap_token'] = token
