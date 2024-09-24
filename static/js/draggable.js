c14eee520effcf6da609edae7ddbcbee8a8a9ca1
dvanael <anael.noki@gmail.com>
2024-09-24 13:20:05 -0300

    feat: adiciona classes ao draggable

--- static/js/draggable.js
+++ static/js/draggable.js
@@ -2,8 +2,15 @@ $(function() {
     const $draggable = $("#draggable");
     const originalPosition = $draggable.position();
 
-    $("#draggable").draggable({
+    $draggable.draggable({
+        start: function() {
+            $("body").addClass("move");
+            $draggable.addClass("box-shadow")
+        },
+
         stop: function() {
+            $("body").removeClass("move");
+            $draggable.removeClass("box-shadow")
             setTimeout(function() {
                 $("#draggable").animate({
                     top: originalPosition.top,
